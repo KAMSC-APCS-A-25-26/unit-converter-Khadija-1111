89a38caad612c75b8b8de048716bbd99115b7259
Khadija-1111 <sfkhadija.edu@gmail.com>
2025-10-03 22:07:05 -0400

    Update DistanceConverter.java

--- src/main/java/DistanceConverter.java
+++ src/main/java/DistanceConverter.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class DistanceConverter {
     public static void main(String[] args) {
         // TODO: Implement distance converter
@@ -7,5 +9,50 @@ public class DistanceConverter {
         // - Use conversion factor: 1 mile = 1.60935 kilometers
         // - Display conversion results
         // - Handle invalid menu choices
+        Scanner scan = new Scanner(System.in);
+        boolean running = true;
+        final double MILES_TO_KM = 1.60935;
+        while(running){
+            System.out.println("\nChoose conversion direction: ");
+            System.out.println("1. Convert Miles to Kilometers");
+            System.out.println("2. Convert Kilometers to Miles");
+            System.out.println("3. Exit");
+            System.out.print("Enter your choice: ");
+            int choice = scan.nextInt();
+            switch (choice){
+                default:
+                    System.out.println("Invalid choice");
+                    break;
+                case 1:
+                    System.out.print("\nEnter distance in miles: ");
+                    double miles1 = scan.nextDouble();
+                    double kilometers1 = miles1 * MILES_TO_KM;
+                    if (miles1 == 0){
+                       System.out.println("Distance is zero. Conversion result will also be zero.");
+                    }
+                    else {
+                        System.out.println("\nConversion result:");
+                        System.out.printf("Miles: %.2f\n", miles1);
+                        System.out.printf("Kilometers: %.2f\n", kilometers1);
+                    }
+                    break;
+                case 2:
+                    System.out.print("\nEnter distance in kilometers: ");
+                    double miles2 = scan.nextDouble();
+                    double kilometers2 = miles2 / MILES_TO_KM;
+                    if (miles2 == 0) {
+                        System.out.println("Distance is zero. Conversion result will also be zero.");
+                    }
+                    else {
+                        System.out.println("\nConversion result:");
+                        System.out.printf("Kilometers: %.2f\n", kilometers2);
+                        System.out.printf("Miles: %.2f\n", miles2);
+                    }
+                    break;
+                case 3:
+                    System.out.println("Goodbye!");
+                    running = false;
+            }
+        }
     }
 }
